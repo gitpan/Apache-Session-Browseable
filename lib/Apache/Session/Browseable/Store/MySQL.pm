3c8c6e21513d1dc67666878279c6fcf666eedc26
Xavier Guimard <x.guimard@free.fr>
2009-10-31 08:09:42 +0000

    Import of GUIMARD/Apache-Session-Browseable-0.1 from CPAN.
    gitpan-cpan-distribution: Apache-Session-Browseable
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         GUIMARD/Apache-Session-Browseable-0.1.tar.gz
    gitpan-cpan-author:       GUIMARD
    gitpan-cpan-maturity:     released

---
+++ lib/Apache/Session/Browseable/Store/MySQL.pm
@@ -0,0 +1,12 @@
+package Apache::Session::Browseable::Store::MySQL;
+
+use strict;
+
+use Apache::Session::Browseable::Store::DBI;
+use Apache::Session::Store::MySQL;
+
+our @ISA =
+  qw(Apache::Session::Browseable::Store::DBI Apache::Session::Store::MySQL);
+
+1;
+
